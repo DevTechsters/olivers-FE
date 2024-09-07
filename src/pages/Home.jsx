ecedf8b7d5aa39bd83a8e9b9d5105b07a0de1703
mohan <mohantcsofficial@gmail.com>
2024-09-07 14:57:21 +0530

    Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,98 @@
 import React from 'react'
 import Header from '../components/Header'
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { DataGrid ,GridActionsCellItem} from '@mui/x-data-grid';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 
 export default function Home() {
+
+  const handleEditClick = (id) => () => {
+  };
+
+  
+
+  const handleDeleteClick = (id) => () => {
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'INVOICE', width: 90 },
+    {
+      field: 'firstName',
+      headerName: 'CUSTOMER',
+    },
+    {
+      field: 'lastName',
+      headerName: 'STATUS',
+    },
+    {
+      field: 'age',
+      headerName: 'AMOUNT',
+      type: 'number',
+    },
+    {
+      field: 'fullName',
+      headerName: 'PAYMENT TYPE',
+    },
+    {
+      field: 'fullName',
+      headerName: 'UPDATED BY',
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
+      width: 100,
+      cellClassName: 'actions',
+      getActions: ({ id }) => {
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+
+  const rows=[
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+  ]
+ 
+
   return (
     <>
       <Header />
-      <Button variant="contained"></Button>
+      <Button variant="contained">Export</Button>
+
+      <Box sx={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
     </>
   )
 }
