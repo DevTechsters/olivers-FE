6daf1ce18ca2b5b9f9c91d9f56b162316090c115
mohan <mohantcsofficial@gmail.com>
2024-09-14 21:45:50 +0530

    modal UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
-import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
@@ -10,8 +9,9 @@ import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import Loader from '../components/Loader';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import { Modal, ModalHeader, ModalBody, ModalFooter, Row, Label, Col, Input } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Row, Label, Col, Input, Button ,Table} from 'reactstrap';
 import axios from 'axios';
+import Select from 'react-select';
 
 export default function Home() {
   const [paginationModel, setPaginationModel] = useState({
@@ -21,11 +21,15 @@ export default function Home() {
   const [editData, setEditdata] = useState({})
   const [loading, setLoading] = useState(false);
   const [modal, setModal] = useState(false);
+  const [BillsHistory,setBillsHistory]=useState([])
 
   const toggle = () => setModal(!modal);
   const handleEditClick = (id) => () => {
     setModal(true)
+    console.log(rows[id]);
+    
     setEditdata(rows[id])
+    setBillsHistory(rows[id].BillsHistory)
   };
 
 
@@ -160,8 +164,89 @@ export default function Home() {
       headerName: 'Balance',
     },
   ];
-
+  console.log(editData,"Edit")
   const rows = [
+    {
+      id: 0,
+      BillId: 1,
+      Brand: "Chocalate",
+      SalespersonName: "Hariii",
+      Beat: "PaperMill",
+      Day: "Thursday",
+      Bill_Number: "FR09876545678",
+      Billdate: "24/9/2024",
+      day_count: 14,
+      RetailerName: "Pandian Stores Super Market",
+      Balance: 1000,
+      Rec: 100,
+      InvoiceAmount: 500,
+      amount_received: 100,
+      Cheque: 0,
+      CashDiscount: 40,
+      Damage: 0,
+      Claim: 100,
+      CreditNote: 100,
+      Gpay: 100,
+      part_payment: 100,
+      delivery: 100,
+      Cancel: 100,
+      createdBy: "Admin",
+      createdAt: "2024-04-26T10:30:00",
+      updatedBy: null,
+      updatedAt: null,
+      tally_status: "completed",
+      BillsHistory: [{
+        date: "26/4/2024",
+        paymentMethod: "Cash, Credit Note",
+        receivedAmoun: 200,
+        comments: "Paid cash and used credit note",
+        createdBy: "Admin",
+        createdAt: "2024-04-26T10:30:00",
+        updatedBy: null,
+        updatedAt: null
+      },
+      {
+        date: "27/4/2024",
+        paymentMethod: "Gpay, Part Payment",
+        receivedAmount: 150,
+        comments: "Part payment through Gpay",
+        createdBy: "Admin",
+        createdAt: "2024-04-27T11:00:00",
+        updatedBy: null,
+        updatedAt: null
+      },
+      {
+        date: "27/4/2024",
+        paymentMethod: "Gpay, Part Payment",
+        receivedAmount: 150,
+        comments: "Part payment through Gpay",
+        createdBy: "Admin",
+        createdAt: "2024-04-27T11:00:00",
+        updatedBy: null,
+        updatedAt: null
+      },
+      {
+        date: "27/4/2024",
+        paymentMethod: "Gpay, Part Payment",
+        receivedAmount: 150,
+        comments: "Part payment through Gpay",
+        createdBy: "Admin",
+        createdAt: "2024-04-27T11:00:00",
+        updatedBy: null,
+        updatedAt: null
+      },
+      {
+        date: "27/4/2024",
+        paymentMethod: "Gpay, Part Payment",
+        receivedAmount: 150,
+        comments: "Part payment through Gpay",
+        createdBy: "Admin",
+        createdAt: "2024-04-27T11:00:00",
+        updatedBy: null,
+        updatedAt: null
+      },
+    ]
+    },
     {
       id: 1,
       BillId: 1,
@@ -169,7 +254,7 @@ export default function Home() {
       SalespersonName: "Hari",
       Beat: "PaperMill",
       Day: "Thursday",
-      Bill_Number: "FR09876545678",
+      Bill_Number: "IO09876545678",
       Billdate: "24/9/2024",
       day_count: 14,
       RetailerName: "Pandian Stores Super Market",
@@ -199,7 +284,7 @@ export default function Home() {
       SalespersonName: "Hari",
       Beat: "PaperMill",
       Day: "Thursday",
-      Bill_Number: "IO09876545678",
+      Bill_Number: "FR09876545678",
       Billdate: "24/9/2024",
       day_count: 14,
       RetailerName: "Pandian Stores Super Market",
@@ -281,36 +366,6 @@ export default function Home() {
       updatedBy: null,
       updatedAt: null,
       tally_status: "completed"
-    },
-    {
-      id: 5,
-      BillId: 1,
-      Brand: "Chocalate",
-      SalespersonName: "Hari",
-      Beat: "PaperMill",
-      Day: "Thursday",
-      Bill_Number: "FR09876545678",
-      Billdate: "24/9/2024",
-      day_count: 14,
-      RetailerName: "Pandian Stores Super Market",
-      Balance: 1000,
-      Rec: 100,
-      InvoiceAmount: 500,
-      amount_received: 100,
-      Cheque: 0,
-      CashDiscount: 40,
-      Damage: 0,
-      Claim: 100,
-      CreditNote: 100,
-      Gpay: 100,
-      part_payment: 100,
-      delivery: 100,
-      Cancel: 100,
-      createdBy: "Admin",
-      createdAt: "2024-04-26T10:30:00",
-      updatedBy: null,
-      updatedAt: null,
-      tally_status: "completed"
     }
   ]
 
@@ -357,22 +412,78 @@ export default function Home() {
             />
           </Box>
         </div>
-        <Modal isOpen={modal} toggle={toggle} centered={true} size='lg'>
+        <Modal isOpen={modal} toggle={toggle} centered={true} size='xl'>
           <ModalHeader toggle={toggle}>{editData.Bill_Number}</ModalHeader>
           <ModalBody>
-            <Row>
-              <Col>
-                <Label>Amount</Label>
-                <Input type='number' min='1' />
-              </Col>
-              <Col>
-                <Label>Comments</Label>
-                <Input type='textarea' min='1' />
-              </Col>
-            </Row>
+            <div className='m-2 p-2'>
+              <Row>
+                <Col>
+                  <Label>Amount</Label>
+                  <Input type='number' min='1' />
+                </Col>
+                <Col>
+                  <Label>Comments</Label>
+                  <Input type='textarea' min='1' />
+                </Col>
+                <Col>
+                  <Label>Payment Method</Label>
+                  <Select
+                    options={[
+                      { value: "Gpay", label: "Gpay" },
+                      { value: "Cash", label: "Cash" },
+                      { value: "Cheque", label: "Cheque" },
+                    ]}
+                  />
+                </Col>
+                <Col>
+                  <Label>Date</Label>
+                  <Input type='date' />
+                </Col>
+                <Col>
+                  <Button color="primary">Add</Button>
+                </Col>
+              </Row>
+            </div>
+            <div className='m-2 p-2'>
+              <Table
+                bordered
+                hover
+                responsive
+                striped
+              >
+                <thead>
+                  <tr>
+                    <th>Data</th>
+                    <th>Payment Method</th>
+                    <th>Received amount</th>
+                    <th>Comments</th>
+                    <th>Created by</th>
+                    <th>Created At</th>
+                    <th>Updated by</th>
+                    <th>Updated At</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {BillsHistory&&BillsHistory.map((item)=>{
+                    return( <tr>
+                      <td>{item.date}</td>
+                      <td>{item.paymentMethod}</td>
+                      <td>{item.receivedAmount}</td>
+                      <td>{item.comments}</td>
+                      <td>{item.createdBy}</td>
+                      <td>{item.createdAt}</td>
+                      <td>{item.updatedBy}</td>
+                      <td>{item.updatedAt}</td>
+                    </tr>)
+                  })}
+                </tbody>
+              </Table>
+            </div>
+
           </ModalBody>
           <ModalFooter>
-
+            <Button color='primary'>Save</Button>
+            <Button onClick={toggle}>Cancel</Button>
           </ModalFooter>
         </Modal>
       </>}
