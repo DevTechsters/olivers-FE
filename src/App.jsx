465dfb6ffe4acd5bbfa09382ecb12c17ed76ebbf
mohan <mohantcsofficial@gmail.com>
2024-09-14 21:03:36 +0530

    Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Provider } from 'react-redux'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import AppRouter from './routes'
 import store from './redux'
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,16 +10,22 @@ import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import Loader from '../components/Loader';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Row, Label, Col, Input } from 'reactstrap';
 import axios from 'axios';
 
 export default function Home() {
-  const [data, setData] = useState([]);
   const [paginationModel, setPaginationModel] = useState({
     page: 0,
     pageSize: 10,
   });
+  const [editData, setEditdata] = useState({})
   const [loading, setLoading] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
   const handleEditClick = (id) => () => {
+    setModal(true)
+    setEditdata(rows[id])
   };
 
 
@@ -28,107 +34,6 @@ export default function Home() {
   };
 
   const columns = [
-    { field: 'Brand', headerName: 'Brand' },
-    {
-      field: 'SalespersonName',
-      headerName: 'Salesperson Name',
-    },
-    {
-      field: 'Beat',
-      headerName: 'BEAT',
-    },
-    {
-      field:"Day",
-      headerName:"Day"
-    },
-    {
-      field:"day_count",
-      headerName:"Day count"
-    },
-    {
-      field: 'Bill_Number',
-      headerName: 'Bill No',
-    },
-    {
-      field: 'Billdate',
-      headerName: 'Bill Date',
-    },
-    {
-      field: 'RetailerName',
-      headerName: 'Retailer Name',
-    },
-    {
-      field:"Rec",
-      headerName:"Rec"
-    },
-    {
-      field: 'InvoicedAmount',
-      headerName: 'Invoice Amount',
-    },
-    {
-      field: 'Amount Received',
-      headerName: 'amount_received',
-    },
-    {
-      field:"Cheque",
-      headerName:"Cheque"
-    },
-    {
-      field: 'CashDiscount',
-      headerName: 'Cash Discount',
-    },
-    {
-      field: 'Damage',
-      headerName: 'Damage',
-    },
-    {
-      field: 'Claim',
-      headerName: 'Claim',
-    },
-    {
-      field: 'CreditNote',
-      headerName: 'CR Note',
-    },
-    {
-      field:"Gpay",
-      headerName:"Gpay"
-    },
-    {
-      field:"part_payment",
-      headerName:"Part Payment"
-    },
-    {
-      field: 'delivery',
-      headerName: 'Delivery Status',
-    },
-    {
-      field:"Cancel",
-      headerName:"Cancel"
-    },
-    {
-      field:"createdBy",
-      headerName:"Created By"
-    },
-    {
-      field:"createdAt",
-      headerName:"Created At"
-    },
-    {
-      field:"Updated By",
-      headerName:"updatedBy"
-    },
-    {
-      field:"updatedAt",
-      headerName:"Updated At"
-    },
-    {
-      field: 'tally_status',
-      headerName: 'Tally Status',
-    },
-    {
-      field: 'Balance',
-      headerName: 'Balance',
-    },
     {
       field: 'actions',
       type: 'actions',
@@ -153,11 +58,112 @@ export default function Home() {
         ];
       },
     },
+    { field: 'Brand', headerName: 'Brand' },
+    {
+      field: 'SalespersonName',
+      headerName: 'Salesperson Name',
+    },
+    {
+      field: 'Beat',
+      headerName: 'BEAT',
+    },
+    {
+      field: "Day",
+      headerName: "Day"
+    },
+    {
+      field: "day_count",
+      headerName: "Day count"
+    },
+    {
+      field: 'Bill_Number',
+      headerName: 'Bill No',
+    },
+    {
+      field: 'Billdate',
+      headerName: 'Bill Date',
+    },
+    {
+      field: 'RetailerName',
+      headerName: 'Retailer Name',
+    },
+    {
+      field: "Rec",
+      headerName: "Rec"
+    },
+    {
+      field: 'InvoicedAmount',
+      headerName: 'Invoice Amount',
+    },
+    {
+      field: 'Amount Received',
+      headerName: 'amount_received',
+    },
+    {
+      field: "Cheque",
+      headerName: "Cheque"
+    },
+    {
+      field: 'CashDiscount',
+      headerName: 'Cash Discount',
+    },
+    {
+      field: 'Damage',
+      headerName: 'Damage',
+    },
+    {
+      field: 'Claim',
+      headerName: 'Claim',
+    },
+    {
+      field: 'CreditNote',
+      headerName: 'CR Note',
+    },
+    {
+      field: "Gpay",
+      headerName: "Gpay"
+    },
+    {
+      field: "part_payment",
+      headerName: "Part Payment"
+    },
+    {
+      field: 'delivery',
+      headerName: 'Delivery Status',
+    },
+    {
+      field: "Cancel",
+      headerName: "Cancel"
+    },
+    {
+      field: "createdBy",
+      headerName: "Created By"
+    },
+    {
+      field: "createdAt",
+      headerName: "Created At"
+    },
+    {
+      field: "Updated By",
+      headerName: "updatedBy"
+    },
+    {
+      field: "updatedAt",
+      headerName: "Updated At"
+    },
+    {
+      field: 'tally_status',
+      headerName: 'Tally Status',
+    },
+    {
+      field: 'Balance',
+      headerName: 'Balance',
+    },
   ];
 
-  const rows =[
+  const rows = [
     {
-      id:1,
+      id: 1,
       BillId: 1,
       Brand: "Chocalate",
       SalespersonName: "Hari",
@@ -187,7 +193,37 @@ export default function Home() {
       tally_status: "completed"
     },
     {
-      id:2,
+      id: 2,
+      BillId: 1,
+      Brand: "Chocalate",
+      SalespersonName: "Hari",
+      Beat: "PaperMill",
+      Day: "Thursday",
+      Bill_Number: "IO09876545678",
+      Billdate: "24/9/2024",
+      day_count: 14,
+      RetailerName: "Pandian Stores Super Market",
+      Balance: 1000,
+      Rec: 100,
+      InvoiceAmount: 500,
+      amount_received: 100,
+      Cheque: 0,
+      CashDiscount: 40,
+      Damage: 0,
+      Claim: 100,
+      CreditNote: 100,
+      Gpay: 100,
+      part_payment: 100,
+      delivery: 100,
+      Cancel: 100,
+      createdBy: "Admin",
+      createdAt: "2024-04-26T10:30:00",
+      updatedBy: null,
+      updatedAt: null,
+      tally_status: "completed"
+    },
+    {
+      id: 3,
       BillId: 1,
       Brand: "Chocalate",
       SalespersonName: "Hari",
@@ -217,7 +253,7 @@ export default function Home() {
       tally_status: "completed"
     },
     {
-      id:3,
+      id: 4,
       BillId: 1,
       Brand: "Chocalate",
       SalespersonName: "Hari",
@@ -247,37 +283,7 @@ export default function Home() {
       tally_status: "completed"
     },
     {
-      id:4,
-      BillId: 1,
-      Brand: "Chocalate",
-      SalespersonName: "Hari",
-      Beat: "PaperMill",
-      Day: "Thursday",
-      Bill_Number: "FR09876545678",
-      Billdate: "24/9/2024",
-      day_count: 14,
-      RetailerName: "Pandian Stores Super Market",
-      Balance: 1000,
-      Rec: 100,
-      InvoiceAmount: 500,
-      amount_received: 100,
-      Cheque: 0,
-      CashDiscount: 40,
-      Damage: 0,
-      Claim: 100,
-      CreditNote: 100,
-      Gpay: 100,
-      part_payment: 100,
-      delivery: 100,
-      Cancel: 100,
-      createdBy: "Admin",
-      createdAt: "2024-04-26T10:30:00",
-      updatedBy: null,
-      updatedAt: null,
-      tally_status: "completed"
-    },
-    {
-      id:5,
+      id: 5,
       BillId: 1,
       Brand: "Chocalate",
       SalespersonName: "Hari",
@@ -306,12 +312,14 @@ export default function Home() {
       updatedAt: null,
       tally_status: "completed"
     }
-  ] 
+  ]
 
 
-  useEffect(()=>{
-    const response= axios.get()
-  },[paginationModel])
+  useEffect(() => {
+    const response = axios.get()
+
+
+  }, [paginationModel])
 
   return (
     <>
@@ -338,18 +346,36 @@ export default function Home() {
             <DataGrid
               rows={rows}
               columns={columns}
-              
+
               loading={loading}
               rowCount={1000}
-              pageSizeOptions={[10,50,100]}
+              pageSizeOptions={[10, 50, 100]}
               paginationModel={paginationModel}
               paginationMode="server"
               onPaginationModelChange={setPaginationModel}
-              checkboxSelection
               disableRowSelectionOnClick
             />
           </Box>
-        </div></>}
+        </div>
+        <Modal isOpen={modal} toggle={toggle} centered={true} size='lg'>
+          <ModalHeader toggle={toggle}>{editData.Bill_Number}</ModalHeader>
+          <ModalBody>
+            <Row>
+              <Col>
+                <Label>Amount</Label>
+                <Input type='number' min='1' />
+              </Col>
+              <Col>
+                <Label>Comments</Label>
+                <Input type='textarea' min='1' />
+              </Col>
+            </Row>
+          </ModalBody>
+          <ModalFooter>
+
+          </ModalFooter>
+        </Modal>
+      </>}
     </>
   )
 }
